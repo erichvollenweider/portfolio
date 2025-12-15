de74e890a12575f10e06d4da1a7b92020c3bcf38
erichvollenweider <vollenweidererich@yahoo.com.ar>
2025-12-15 16:45:45 -0300

    Add Railway configuration

--- rxconfig.py
+++ rxconfig.py
@@ -2,8 +2,5 @@ import reflex as rx
 
 config = rx.Config(
     app_name="portfolio",
-    plugins=[
-        rx.plugins.SitemapPlugin(),
-        rx.plugins.TailwindV4Plugin(),
-    ]
+    show_built_with_reflex=False
 )
